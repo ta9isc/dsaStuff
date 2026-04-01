58b7e0664882d44f4f78009d8d725b82143c30f4
ta9isc <tanishc880@gmail.com>
2026-04-01 15:45:22 +0530

    Codeforces Problem My first sorting program

---
+++ CF1971A.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class CF1971A{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+            int x , y;
+            x = sc.nextInt();
+            y = sc.nextInt();
+
+            System.out.println( Math.min(x, y)+ " " + Math.max(x, y));
+            t -= 1;
+        }
+    }
+}
